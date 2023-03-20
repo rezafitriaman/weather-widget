4ef5fbbd824c8c84f423fdd45081fec170c56031
Reza Fitriaman <reza.fitriaman@quli.nl>
2023-03-20 22:34:29 +0100

    delete unused import

--- weather-widget/src/app/services/weather.service.spec.ts
+++ weather-widget/src/app/services/weather.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { WeatherService } from './weather.service';
 import { HttpClient } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('WeatherService', () => {
   let service: WeatherService;
